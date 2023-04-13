364ccb3a6952cc563ba46c18916d80342fc4bc07
Arne Martin Aurlien <arnemart@Ebba.local>
2023-04-13 22:07:19 +0200

    yolo

--- vsmp.py
+++ vsmp.py
@@ -11,7 +11,7 @@ from waveshare_epd import epd7in5_V2
 
 logging.basicConfig(level=logging.ERROR)
 
-
+logging.info("Load status JSON")
 with open("status.json") as status_file:
   status = json.load(status_file)
 
@@ -19,18 +19,23 @@ prev_frame = path.join(dir, "movies", status["movie"], "frame_%d.png" % (status[
 
 status["frame"] += 1
 
+logging.debug("Current movie is %s, next frame is %d" % (status["movie"], status["frame"]))
+
 next_frame = path.join(dir, "movies", status["movie"], "frame_%d.png" % (status["frame"]))
 
 if not path.exists(next_frame):
+  logging.debug("Image file for next frame does not exists, go to next movie")
   movies = listdir("movies")
   movies.sort()
   if status["movie"] in movies:
     status["movie"] = movies[(movies.index(status["movie"]) + 1) % len(movies)]
   else:
     status["movie"] = movies[0]
+  logging.debug("Next movie is %s" % (status["movie"]))
   status["frame"] = 1
   next_frame = path.join(dir, "movies", status["movie"], "frame_1.png")
 
+logging.info("Save new status JSON")
 with open("status.json", "w") as status_file:
   json.dump(status, status_file)
 
@@ -40,10 +45,11 @@ try:
   if list(prev_img.getdata()) == list(img.getdata()):
     logging.info("Frames are identical, do nothing")
   else:
+    logging.info("Display next frame")
     epd = epd7in5_V2.EPD()
     epd.init()
     epd.display(epd.getbuffer(img))
     epd.sleep()
-    
+
 except IOError as e:
-  logging.info(e)
+  logging.error(e)
